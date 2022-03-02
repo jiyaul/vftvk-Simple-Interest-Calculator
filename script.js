39b65807d75c645533048542a5aab11e3b46f58d
Jiyaul Haq <jiyahul786@gmail.com>
2022-03-02 10:36:30 +0530

    Jiyaul Haq

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute(){
+    var principal = document.getElementById("principal").value;
     
+    if(principal<=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);    
+    var result ="If you deposit <b>"+principal+"</b>,";
+    result +="<br>at an interest rate of <b>"+rate+"</b>.";
+    result+="<br>You will receive an amount of <b>"+interest+"</b>, in year <b>"+year+"</b>";
+    document.getElementById("result").innerHTML=result;
 }
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
         
